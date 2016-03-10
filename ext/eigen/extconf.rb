160a20e927b20080e85cb18e12800efaf63681ab
Takashi Tamura <tamuratak@users.noreply.github.com>
2016-03-10 21:53:59 +0900

    add eigen path to include option

--- ext/eigen/extconf.rb
+++ ext/eigen/extconf.rb
@@ -2,6 +2,6 @@ require 'mkmf'
 have_library("c++") or have_library("stdc++")
 
 cppad_path = File.join(File.dirname(File.expand_path(__FILE__)), "../cppad/cppad/")
-ruby_eigen = File.join(File.dirname(File.expand_path(__FILE__)), "ruby-eigen")
-$CXXFLAGS = ($CXXFLAGS || "") + " -O2 -Wall -I #{cppad_path} "
+ruby_eigen_path = File.join(File.dirname(File.expand_path(__FILE__)), "ruby-eigen/ext/eigen/eigen3")
+$CXXFLAGS = ($CXXFLAGS || "") + " -O2 -Wall -I #{cppad_path} -I #{ruby_eigen_path} "
 create_makefile('cppad/eigen')
